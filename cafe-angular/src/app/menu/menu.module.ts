648c812b392271799b90b9418c1a273824291455
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-07 19:10:00 +0300

    Update menu.module.ts

--- cafe-angular/src/app/menu/menu.module.ts
+++ cafe-angular/src/app/menu/menu.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CategoryList } from './category-list/category-list.component';
 import { DishesList } from './dishes-list/dishes-list.component';
+import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
   declarations: [CategoryList, DishesList],
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
   exports: [CategoryList, DishesList],
   providers: [],
   bootstrap: [],
