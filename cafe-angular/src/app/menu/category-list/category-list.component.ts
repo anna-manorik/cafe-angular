d7a693b6b574469b5d94d65ee731d7e6365caa3c
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-07 15:25:25 +0300

    Create category-list.component.ts

---
+++ cafe-angular/src/app/menu/category-list/category-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {DataService} from '../../core/services/data.service';
+
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.css'],
+  providers:[DataService],
+})
+
+export class CategoryList implements OnInit {
+  title = 'category-list';
+  categories: any;
+
+  constructor(public service: DataService) { }
+
+  @Output() onClick = new EventEmitter();
+
+  ngOnInit() {
+    this.service.getCategories().subscribe((response: any) => {
+      this.categories = response;
+    });
+  }
+
+  getDishes(categoryName: any) {
+    // this.service.getDishes(categoryName);
+
+    this.onClick.emit(categoryName);
+  }
+
+}
