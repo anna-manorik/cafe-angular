4cee66d3be58695c705707a4040817dfc072b421
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-08 16:22:47 +0300

    Update header.component.ts

--- cafe-angular/src/app/core/components/header/header.component.ts
+++ cafe-angular/src/app/core/components/header/header.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DataService } from '../../services/data.service';
+import { DishAddingModal } from '../../../menu/dialogs/dish-adding-modal/dish-adding-modal.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-
 export class Header {
   title = 'Cafe-Angular';
+
+  constructor(public service: DataService, public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(DishAddingModal);
+  }
 }
