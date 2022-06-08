4417ba6b055062a0395975cf07b372d93e983fff
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-08 16:22:37 +0300

    Update app-routing.module.ts

--- cafe-angular/src/app/app-routing.module.ts
+++ cafe-angular/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DishesList } from './menu/dishes-list/dishes-list.component';
+import { PageNotFound } from './menu/pages/notFoundPage/pageNotFound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DishesList },
+  { path: 'all-dishes', component: DishesList },
+  { path: 'dishes/:id', component: DishesList },
+  { path: '**', component: PageNotFound },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
