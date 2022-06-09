ba3a07df6919110d1ff418b871895e346dc5d72e
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-09 12:29:24 +0300

    Update core.module.ts

--- cafe-angular/src/app/core/core.module.ts
+++ cafe-angular/src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Header } from './components/header/header.component';
 import { Footer } from './components/footer/footer.component';
 import { MaterialModule } from '../shared/material.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [Header, Footer],
-  imports: [BrowserModule, MaterialModule],
+  imports: [BrowserModule, MaterialModule, FormsModule, ReactiveFormsModule],
   exports: [Header, Footer],
   providers: [],
   bootstrap: [],
