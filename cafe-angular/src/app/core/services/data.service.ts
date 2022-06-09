cdefa6f42e1ad3f98caac1912df6d56a7e8c1c00
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-09 12:29:27 +0300

    Update data.service.ts

--- cafe-angular/src/app/core/services/data.service.ts
+++ cafe-angular/src/app/core/services/data.service.ts
@@ -1,36 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../../shared/classes/category/category';
+import { Dish } from '../../shared/classes/dish/dish';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class DataService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getCategories() {
-    return this.http
-    .get('http://localhost:3000/categories');
+    return this.http.get('http://localhost:3000/categories');
   }
 
-  getCategoryName(categoryId: any) {
-    return this.http
-    .get(`http://localhost:3000/categories?id=${categoryId}`);
+  getCategoryById(categoryId: any) {
+    return this.http.get(`http://localhost:3000/categories?id=${categoryId}`);
   }
 
   getAllDishes() {
-    return this.http
-    .get('http://localhost:3000/dishes')
+    return this.http.get('http://localhost:3000/dishes');
   }
 
   getDishes(categoryId: any) {
-    return this.http
-    .get(`http://localhost:3000/dishes?categoryId=${categoryId}`)
+    return this.http.get(
+      `http://localhost:3000/dishes?categoryId=${categoryId}`
+    );
   }
 
   getDishInfo(dishId: any) {
-    return this.http
-    .get(`http://localhost:3000/dishes?id=${dishId}`)
+    return this.http.get(`http://localhost:3000/dishes?id=${dishId}`);
+  }
+
+  addCategory(categoryForm: Category) {
+    return this.http.post('http://localhost:3000/categories', categoryForm);
+  }
+
+  addDish(dishForm: Dish) {
+    return this.http.post('http://localhost:3000/dishes', dishForm);
   }
 }
