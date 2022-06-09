fde2c14e59964b1b1533077067550cab2c087aa0
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-09 12:30:06 +0300

    Update menu.module.ts

--- cafe-angular/src/app/menu/menu.module.ts
+++ cafe-angular/src/app/menu/menu.module.ts
@@ -1,15 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { CategoryList } from './category-list/category-list.component';
 import { DishesList } from './dishes-list/dishes-list.component';
+import { DishInfoModal } from './dialogs/dish-info-modal/dish-info-modal.component';
+import { DishAddingModal } from './dialogs/dish-adding-modal/dish-adding-modal.component';
+import { CategoryAddingModal } from './dialogs/category-adding-modal/category-adding-modal.component';
 import { MaterialModule } from '../shared/material.module';
-import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [CategoryList, DishesList],
-  imports: [CommonModule, MaterialModule, RouterModule],
-  exports: [CategoryList, DishesList],
+  declarations: [
+    CategoryList,
+    DishesList,
+    DishAddingModal,
+    CategoryAddingModal,
+    DishInfoModal,
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    CategoryList,
+    DishesList,
+    DishAddingModal,
+    CategoryAddingModal,
+    DishInfoModal,
+  ],
   providers: [],
   bootstrap: [],
 })
