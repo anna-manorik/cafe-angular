6f21f89b9129c81677a36a9c684e7626586e1c15
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-09 12:29:07 +0300

    Update app.module.ts

--- cafe-angular/src/app/app.module.ts
+++ cafe-angular/src/app/app.module.ts
@@ -5,19 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { MenuModule } from './menu/menu.module';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
     MenuModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent],
