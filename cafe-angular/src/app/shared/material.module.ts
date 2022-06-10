83fe679a5f8a5522fddaf00ddfee11b3086afbdb
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-10 12:08:25 +0300

    Update material.module.ts

--- cafe-angular/src/app/shared/material.module.ts
+++ cafe-angular/src/app/shared/material.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [],
-  imports: [MatButtonModule, MatTableModule, MatDialogModule],
-  exports: [MatButtonModule, MatTableModule, MatDialogModule],
+  imports: [MatButtonModule, MatTableModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatSelectModule],
+  exports: [MatButtonModule, MatTableModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatSelectModule],
   providers: [],
   bootstrap: [],
 })
