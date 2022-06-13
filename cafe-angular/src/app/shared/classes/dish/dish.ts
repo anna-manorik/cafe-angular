16a0a398fc13f924c1e185754e14a9f4e89c2122
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-13 19:56:26 +0300

    Update dish.ts

--- cafe-angular/src/app/shared/classes/dish/dish.ts
+++ cafe-angular/src/app/shared/classes/dish/dish.ts
@@ -1,7 +1,7 @@
 export class Dish {
-    // id: number = 0;
-    title:string = '';
-    discription: string = '';
-    price: number = 0;
-    categoryId: number = 0;
-  }
+  id?: number;
+  title!: string;
+  discription!: string;
+  price!: number;
+  categoryId!: number;
+}
