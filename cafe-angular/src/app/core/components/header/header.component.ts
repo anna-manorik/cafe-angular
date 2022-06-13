1a3d3f72a6218b1adf69b6c4df098cb48c6da424
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-13 19:55:23 +0300

    Update header.component.ts

--- cafe-angular/src/app/core/components/header/header.component.ts
+++ cafe-angular/src/app/core/components/header/header.component.ts
@@ -1,34 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DataService } from '../../services/data.service';
 import { CategoryAddingModal } from '../../../menu/dialogs/category-adding-modal/category-adding-modal.component';
 import { DishAddingModal } from '../../../menu/dialogs/dish-adding-modal/dish-adding-modal.component';
+import { fromEvent, delay, ReplaySubject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class Header {
+export class Header implements OnDestroy {
   title = 'Cafe-Angular';
-  categories: any;
+  searchValue: string = '';
+  destroy: ReplaySubject<any> = new ReplaySubject<any>(1);
+  data: any = fromEvent(document, 'change');
 
   constructor(public service: DataService, public dialog: MatDialog) {}
 
+  ngOnDestroy() {
+    this.destroy.next(null);
+    this.destroy.complete();
+  }
+
   openDialogCategory() {
     this.dialog.open(CategoryAddingModal);
-
-    // dialogRef.afterClosed().subscribe((result) => {
-    //   this.service.getCategories().subscribe((response: any) => {
-    //     // console.log(response)
-    //     // console.log(this.service.updateCategories);
-    //     // this.service.updateCategories.next(response);
-    //     // console.log(this.service.updateCategories);
-    //   });
-    // });
   }
 
   openDialogDish() {
     this.dialog.open(DishAddingModal);
+  }
+
+  searchDishes(searchValue: string) {
+    
+
+    this.data.pipe(delay(1000), takeUntil(this.destroy))
+      .subscribe(() => {
+        this.service.searchDishes.next(searchValue);
+      });
   }
 }
