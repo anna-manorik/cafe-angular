2269772aae642b45701695e33f24b63094eab797
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-13 19:56:14 +0300

    Update dishes-list.component.ts

--- cafe-angular/src/app/menu/dishes-list/dishes-list.component.ts
+++ cafe-angular/src/app/menu/dishes-list/dishes-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../../core/services/data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DishInfoModal } from '../dialogs/dish-info-modal/dish-info-modal.component';
+import { Subject, takeUntil } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dishes-list',
@@ -9,55 +11,73 @@ import { DishInfoModal } from '../dialogs/dish-info-modal/dish-info-modal.compon
   styleUrls: ['./dishes-list.component.css'],
   // providers: [DataService],
 })
-export class DishesList implements OnInit {
-  title: any;
+export class DishesList implements OnInit, OnDestroy {
+  title: string = '';
   dishes: any;
+  destroy: Subject<void> = new Subject<void>();
 
-  constructor(public service: DataService, public dialog: MatDialog) {}
+  constructor(public service: DataService, public dialog: MatDialog, private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
-    this.getAllDishes();
+    console.log(this.activatedRoute.snapshot.params['id'])
+    console.log('this.router.url', this.router.url)
 
-    this.service.updateDishes.subscribe(() => {
+    if(this.router.url === '/all-dishes'){
+      this.getAllDishes();
+
+      this.service.searchDishes.pipe(takeUntil(this.destroy)).subscribe((searchValue) => {
+        this.service.searchDish(searchValue).pipe(takeUntil(this.destroy)).subscribe((response) => {
+          this.dishes = response;
+        });
+      }); // !!!! switchMap or mergeMap
+    } else {
+      const categoryId : number = this.activatedRoute.snapshot.params['id'];
+      this.getDishes(categoryId);
+    }
+    
+
+    this.service.updateDishes.pipe(takeUntil(this.destroy)).subscribe(() => {
       this.getAllDishes();
     });
   }
 
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.complete();
+  }
+
   getAllDishes() {
-    this.service.getAllDishes().subscribe((response) => {
+    this.service.getAllDishes().pipe(takeUntil(this.destroy)).subscribe((response) => {
       this.dishes = response;
       this.title = 'All';
     });
   }
 
-  getDishes(categoryId: any) {
-    this.service.getDishes(categoryId).subscribe((response) => {
+  getDishes(categoryId: number) {
+    this.service.getDishes(categoryId).pipe(takeUntil(this.destroy)).subscribe((response) => {
       this.dishes = response;
     });
 
     this.service.updateDishes.subscribe(() => {
-      this.service.getDishes(categoryId).subscribe((response) => {
+      this.service.getDishes(categoryId).pipe(takeUntil(this.destroy)).subscribe((response) => {
         this.dishes = response;
       });
     });
 
-    this.service.getCategoryById(categoryId).subscribe((response: any) => {
+    this.service.getCategoryById(categoryId).pipe(takeUntil(this.destroy)).subscribe((response: any) => {
       this.title = response[0].title;
     });
   }
 
-  openDialog(dishId: any) {
-    let dialogRef = this.dialog.open(DishInfoModal, {
+  openDialog(dishId: number) {
+    this.dialog.open(DishInfoModal, {
       data: dishId,
     });
-    dialogRef.componentInstance.dishId = dishId; // !!!! remove
   }
 
   deleteDish(dishId: number) {
-    console.log(dishId);
-    this.service.deleteDish(dishId).subscribe(() =>{
+    this.service.deleteDish(dishId).pipe(takeUntil(this.destroy)).subscribe(() => {
       this.service.updateDishes.next();
     });
-    
   }
 }
