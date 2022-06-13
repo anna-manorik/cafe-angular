6e7f5d5e8f3fb394331c332c975a4d796f27aead
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-13 21:44:25 +0300

    Update dishes-list.component.ts

--- cafe-angular/src/app/menu/dishes-list/dishes-list.component.ts
+++ cafe-angular/src/app/menu/dishes-list/dishes-list.component.ts
@@ -2,39 +2,63 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../../core/services/data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DishInfoModal } from '../dialogs/dish-info-modal/dish-info-modal.component';
-import { Subject, takeUntil } from 'rxjs';
+import { switchMap, of, Subject, takeUntil } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Dish } from 'src/app/shared/classes/dish/dish';
 
 @Component({
   selector: 'app-dishes-list',
   templateUrl: './dishes-list.component.html',
   styleUrls: ['./dishes-list.component.css'],
-  // providers: [DataService],
 })
 export class DishesList implements OnInit, OnDestroy {
   title: string = '';
-  dishes: any;
+  dishes!: Dish[];
   destroy: Subject<void> = new Subject<void>();
 
-  constructor(public service: DataService, public dialog: MatDialog, private router: Router, private activatedRoute: ActivatedRoute) {}
+  constructor(
+    public service: DataService,
+    public dialog: MatDialog,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    console.log(this.activatedRoute.snapshot.params['id'])
-    console.log('this.router.url', this.router.url)
+    // console.log(this.activatedRoute.snapshot.params['id'])
+    // console.log('this.router.url', this.router.url)
 
-    if(this.router.url === '/all-dishes'){
+    if (this.router.url === '/all-dishes') {
       this.getAllDishes();
 
-      this.service.searchDishes.pipe(takeUntil(this.destroy)).subscribe((searchValue) => {
-        this.service.searchDish(searchValue).pipe(takeUntil(this.destroy)).subscribe((response) => {
-          this.dishes = response;
+      this.service.searchDishes
+        .pipe(takeUntil(this.destroy))
+        .subscribe((searchValue) => {
+          this.service
+            .searchDish(searchValue)
+            .pipe(takeUntil(this.destroy))
+            .subscribe((response) => {
+              this.dishes = response;
+            });
         });
-      }); // !!!! switchMap or mergeMap
+
+      const switched = this.service.searchDishes.pipe(
+        switchMap((searchValue) =>
+          of(
+            this.service
+              .searchDish(searchValue)
+              .pipe(takeUntil(this.destroy))
+              .subscribe((response) => {
+                this.dishes = response;
+              })
+          )
+        )
+      );
+      
+      // switched.subscribe(x => console.log(x));
     } else {
-      const categoryId : number = this.activatedRoute.snapshot.params['id'];
+      const categoryId: number = this.activatedRoute.snapshot.params['id'];
       this.getDishes(categoryId);
     }
-    
 
     this.service.updateDishes.pipe(takeUntil(this.destroy)).subscribe(() => {
       this.getAllDishes();
@@ -46,38 +70,53 @@ export class DishesList implements OnInit, OnDestroy {
     this.destroy.complete();
   }
 
-  getAllDishes() {
-    this.service.getAllDishes().pipe(takeUntil(this.destroy)).subscribe((response) => {
-      this.dishes = response;
-      this.title = 'All';
-    });
+  public getAllDishes(): void {
+    this.service
+      .getAllDishes()
+      .pipe(takeUntil(this.destroy))
+      .subscribe((response) => {
+        this.dishes = response;
+        this.title = 'All';
+      });
   }
 
-  getDishes(categoryId: number) {
-    this.service.getDishes(categoryId).pipe(takeUntil(this.destroy)).subscribe((response) => {
-      this.dishes = response;
-    });
-
-    this.service.updateDishes.subscribe(() => {
-      this.service.getDishes(categoryId).pipe(takeUntil(this.destroy)).subscribe((response) => {
+  public getDishes(categoryId: number): void {
+    this.service
+      .getDishes(categoryId)
+      .pipe(takeUntil(this.destroy))
+      .subscribe((response) => {
         this.dishes = response;
       });
+
+    this.service.updateDishes.subscribe(() => {
+      this.service
+        .getDishes(categoryId)
+        .pipe(takeUntil(this.destroy))
+        .subscribe((response) => {
+          this.dishes = response;
+        });
     });
 
-    this.service.getCategoryById(categoryId).pipe(takeUntil(this.destroy)).subscribe((response: any) => {
-      this.title = response[0].title;
-    });
+    this.service
+      .getCategoryById(categoryId)
+      .pipe(takeUntil(this.destroy))
+      .subscribe((response: any) => {
+        this.title = response[0].title;
+      });
   }
 
-  openDialog(dishId: number) {
+  public openDialog(dishId: number | undefined): void {
     this.dialog.open(DishInfoModal, {
       data: dishId,
     });
   }
 
-  deleteDish(dishId: number) {
-    this.service.deleteDish(dishId).pipe(takeUntil(this.destroy)).subscribe(() => {
-      this.service.updateDishes.next();
-    });
+  public deleteDish(dishId: number | undefined): void {
+    this.service
+      .deleteDish(dishId)
+      .pipe(takeUntil(this.destroy))
+      .subscribe(() => {
+        this.service.updateDishes.next();
+      });
   }
 }
