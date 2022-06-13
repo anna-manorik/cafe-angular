5d4c237ca4581e02eafbcc04ca126fcd5625a874
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-13 19:56:03 +0300

    Update dish-info-modal.component.ts

--- cafe-angular/src/app/menu/dialogs/dish-info-modal/dish-info-modal.component.ts
+++ cafe-angular/src/app/menu/dialogs/dish-info-modal/dish-info-modal.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { DataService } from '../../../core/services/data.service';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ReplaySubject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'dish-info-modal',
@@ -9,16 +10,21 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 
 export class DishInfoModal {
-  dishId: any;
+  // dishId: number = 0;
   dishInfo: any;
+  destroy: ReplaySubject<any> = new ReplaySubject<any>(1);
 
-  constructor(public service: DataService, public dialogRef: MatDialogRef<DishInfoModal>) {}
+  constructor(public service: DataService, public dialogRef: MatDialogRef<DishInfoModal>, @Inject(MAT_DIALOG_DATA) public dishId: number) {}
 
   ngOnInit() {
-    this.service.getDishInfo(this.dishId).subscribe((response: any) => {
+    this.service.getDishInfo(this.dishId).pipe(takeUntil(this.destroy)).subscribe((response: any) => {
       this.dishInfo = response[0];
-      console.log(response);
     });
+  }
+
+  ngOnDestroy() {
+    this.destroy.next(null);
+    this.destroy.complete();
   }
 
   closeDialog(): void {
