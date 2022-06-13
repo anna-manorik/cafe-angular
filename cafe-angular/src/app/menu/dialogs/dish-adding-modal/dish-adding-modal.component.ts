16cea684584ea42db0480171d652b2df6f632d50
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-06-13 19:55:56 +0300

    Update dish-adding-modal.component.ts

--- cafe-angular/src/app/menu/dialogs/dish-adding-modal/dish-adding-modal.component.ts
+++ cafe-angular/src/app/menu/dialogs/dish-adding-modal/dish-adding-modal.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../../../core/services/data.service';
 import { MatDialogRef } from '@angular/material/dialog';
-import { Dish } from '../../../shared/classes/dish/dish';
+import { Subject, takeUntil } from 'rxjs';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+import { Category } from 'src/app/shared/classes/category/category';
 
 @Component({
   selector: 'dish-adding-modal',
   templateUrl: 'dish-adding-modal.component.html',
   styleUrls: ['./dish-adding-modal.component.css'],
 })
-
-export class DishAddingModal implements OnInit {
-  dish = new Dish();
-  categories: any;
+export class DishAddingModal implements OnInit, OnDestroy {
+  dishForm: FormGroup = new FormGroup({});
+  categories!: Category[];
+  destroy: Subject<void> = new Subject<void>();
 
   constructor(
     public service: DataService,
@@ -19,18 +26,41 @@ export class DishAddingModal implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.service.getCategories().subscribe((response: any) => {
-      this.categories = response;
+    this.service
+      .getCategories()
+      .pipe(takeUntil(this.destroy))
+      .subscribe((response) => {
+        this.categories = response;
+      });
+
+    this.dishForm = new FormGroup({
+      title: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9_-]{1,50}$'), Validators.maxLength(5)]),
+      price: new FormControl('', Validators.required),
+      categoryId: new FormControl(),
+      discription: new FormControl(),
     });
   }
 
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.complete();
+  }
+
   addDish() {
-    this.service.addDish(this.dish).subscribe(() => {
+    this.service.addDish(this.dishForm.value).subscribe(() => {
       this.service.updateDishes.next();
     });
   }
 
   closeDialog(): void {
     this.dialogRef.close();
+  }
+
+  get title(): AbstractControl {
+    return this.dishForm.get('title') as AbstractControl;
+  }
+
+  get price(): AbstractControl {
+    return this.dishForm.get('price') as AbstractControl;
   }
 }
